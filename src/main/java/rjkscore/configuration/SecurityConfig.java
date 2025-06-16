6d5af73fd293fa8d6f7e0657e392ee1f9c705d6c
kevinPaul21 <kevinpts06@gmail.com>
2025-06-16 13:47:24 +0200

    configuracion segurity

--- src/main/java/rjkscore/configuration/SecurityConfig.java
+++ src/main/java/rjkscore/configuration/SecurityConfig.java
@@ -34,7 +34,8 @@ public class SecurityConfig {
             .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/register", "/api/auth/login", "/api/leagues").permitAll()
+                .requestMatchers("/api/**").permitAll()
+                //  .requestMatchers("/api/auth/register", "/api/auth/login", "/api/panda").permitAll()
                 .anyRequest().authenticated()
             )
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
